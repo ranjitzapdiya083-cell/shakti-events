65e046248f374911812cb7555baec4703866ccb5
ranjitzapdiya083-cell <ranjitzapdiya083@gmail.com>
2026-07-21 07:28:04 +0530

    Add Messages link to AdminSidebar

--- components/AdminSidebar.js
+++ components/AdminSidebar.js
@@ -6,6 +6,7 @@ export default function AdminSidebar() {
       <h2>Shakti Admin</h2>
       <Link href="/admin/dashboard">Dashboard</Link>
       <Link href="/admin/bookings">Bookings</Link>
+      <Link href="/admin/messages">Messages</Link>
       <Link href="/admin/users">Users</Link>
       <Link href="/admin/services">Services</Link>
       <Link href="/admin/gallery">Gallery</Link>
